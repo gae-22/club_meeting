0e99e67e15ba12f8fa4568663a8556e908884b87
gae <gaedev22@gmail.com>
2023-06-13 21:04:52 +0900

    Update agenda.py

--- meeting/models/agenda.py
+++ meeting/models/agenda.py
@@ -1,10 +1,9 @@
 from meeting.models.thread import update
 
-BBS_ARRAY = update()
-length = len(BBS_ARRAY)
-
 
 def get_agendas():
+    BBS_ARRAY = update()
+    length = len(BBS_ARRAY)
     agendas = []
     for i in range(0, length):
         element = BBS_ARRAY[length - i - 1][0]
