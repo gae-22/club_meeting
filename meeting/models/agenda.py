e0092f5cc2e7c1592f54b53e57e4b77c6db97659
gae <gaedev22@gmail.com>
2023-06-26 14:55:57 +0900

    change how to get agendas into prefix match

--- meeting/models/agenda.py
+++ meeting/models/agenda.py
@@ -1,4 +1,5 @@
 from meeting.models.thread import update
+import re
 
 
 def get_agendas():
@@ -7,8 +8,15 @@ def get_agendas():
     agendas = []
     for i in range(0, length):
         element = BBS_ARRAY[length - i - 1][0]
-        if element.startswith("--"):
+        if re.match("--", element):
             agendas = BBS_ARRAY[length - i - 2 : length]
             break
+
+    if agendas == []:
+        for i in range(0, length):
+            element = BBS_ARRAY[length - i - 1][0]
+            if re.match(" --", element):
+                agendas = BBS_ARRAY[length - i - 2 : length]
+                break
 
     return agendas
