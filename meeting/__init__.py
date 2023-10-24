1b9f181484c7c7f6dd694f18d7c42ddcc714fc12
gae <gaedev22@gmail.com>
2023-10-24 23:13:52 +0900

    light modification

--- meeting/__init__.py
+++ meeting/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
 
 app = Flask(__name__)
-app.config.from_object("meeting.config")
+app.config["DEBUG"] = True
+DEBUG = True
+if __name__ == "__main__":
+    app.run()
+
 
 import meeting.views
