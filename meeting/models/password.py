d7b7baf63cb845118f97d26a49cc856b186a6f59
gae <gaedev22@gmail.com>
2023-10-25 11:35:36 +0900

    fix password from 16:40 to 23:59 on Monday

--- meeting/models/password.py
+++ meeting/models/password.py
@@ -5,13 +5,13 @@ password_past = ""
 
 
 def reload_time():
-    mtg_time = datetime.time(16, 40, 0)
+    mtg_time = datetime.time(16, 40, 00)
     mtg_wkdy = 0
 
     now_time = datetime.datetime.now().time()
     now_wkdy = datetime.datetime.now().weekday()
 
-    return True if now_wkdy == mtg_wkdy and now_time < mtg_time else False
+    return False if now_time > mtg_time and now_wkdy == mtg_wkdy else True
 
 
 def randomname(n):
