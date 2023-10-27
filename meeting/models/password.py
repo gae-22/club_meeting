6494c5f15aa9cd98037bcf63d25f272fc96a154c
gae <gaedev22@gmail.com>
2023-10-27 13:29:53 +0900

    add desciption

--- meeting/models/password.py
+++ meeting/models/password.py
@@ -4,7 +4,7 @@ from meeting.models.spread import write_password
 password_past = ""
 
 
-def reload_time():  # 時刻を確認して部会前かどうかを判定
+def reload_time():
     mtg_time = datetime.time(16, 40, 00)
     mtg_wkdy = 0
 
@@ -14,10 +14,9 @@ def reload_time():  # 時刻を確認して部会前かどうかを判定
     return True if now_time > mtg_time and now_wkdy == mtg_wkdy else False
 
 
-def choose_random():  # ファイルからランダムにパスワードを選択
+def choose_random():
     pass_path = "./datas/password.txt"
 
-    # ファイルから読み込み
     with open(pass_path, "r") as f:
         lines = f.read().splitlines()
 
@@ -30,13 +29,12 @@ def randomname(n):
     global password_past
     password = ""
 
-    if reload_time():  # 月曜16:40以降にはパスワードは変更しない
+    if reload_time():
         password = password_past
-    elif password == "":  # 月曜16:40以前にはパスワードを変更する
+    elif password == "":
         password = choose_random()
 
     if password == 1:
-        # ランダム生成
         randlst = [
             random.choice(string.ascii_lowercase + string.digits) for i in range(n)
         ]
